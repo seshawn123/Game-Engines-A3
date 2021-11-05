fbe2d957e35db8941f3bf082dce65e56b83b762e
Unknown <seshawn.suresh@ontariotechu.net>
2021-11-04 22:18:44 -0400

    Update 1.3
    - Added Enemies
    - Modified Bullet Script to also destroy enemies tags on collision

--- Assets/Scripts/Object Pool/Bullet.cs
+++ Assets/Scripts/Object Pool/Bullet.cs
@@ -6,7 +6,12 @@ public class Bullet : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag != "Coin")
+        if (collision.gameObject.tag == "Robot")
+        {
+            BasicPool.Instance.AddPool(gameObject);
+            Destroy(collision.gameObject);
+        }
+        else
         {
             BasicPool.Instance.AddPool(gameObject);
         }
